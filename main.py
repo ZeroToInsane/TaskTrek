73be52db3c6d4a679a64daa00507c2aaba9671f8
ZeroToInsane <mario.spoerl@mail.de>
2023-11-27 21:08:33 +0100

    Added first stuff for Add a Task

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-# Projekt TaskTrek
-# Main Datei
+# Project TaskTrek
+# Main File
+import tkinter as tk
 
 def main():
-	print("Hallo Welt!")
-	# Hier kommt Code hin
+	# Event to add a task
+	def buttonAdd_click():
+		task = inputbox.get()
+		print(task)
+
+	# Main Window
+	window = tk.Tk()
+	window.title("TaskTrek")
+	
+	# Input & Button element for a new task
+	inputbox = tk.Entry(window)	
+	inputbox.pack()
+
+	buttonAdd = tk.Button(window, text="Add", command=buttonAdd_click)
+	buttonAdd.pack()
+
+	window.mainloop()
 
 if __name__ == "__main__":
 	main()
-
 
