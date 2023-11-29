fce8a5929f93a91300a312906f98cf57387b664a
ZeroToInsane <mario.spoerl@mail.de>
2023-11-29 22:06:02 +0100

    Added a save to Json function

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 # Project TaskTrek
 # Main File
 import tkinter as tk
+import json
 
 def main():
 	# Event to add a task
 	def buttonAdd_click():
-		task = inputbox.get()
-		print(task)
+		task = inputBox.get()
+		if task: #Check if the inputBox field is empty
+			listBox.insert(tk.END, task)
+			inputBox.delete(0, tk.END) #Remove the content from input field
+	
+	# Event to save to a Json file
+	def buttonSaveJson_click():
+		taskList = [listBox.get(i) for i in range(listBox.size())]
+		with open('aufgaben.json', 'w') as file:
+			json.dump(taskList, file)
+		print("Liste gespeichert!")
 
 	# Main Window
 	window = tk.Tk()
 	window.title("TaskTrek")
 	
 	# Input & Button element for a new task
-	inputbox = tk.Entry(window)	
-	inputbox.pack()
+	inputBox = tk.Entry(window)	
+	inputBox.pack()
 
 	buttonAdd = tk.Button(window, text="Add", command=buttonAdd_click)
 	buttonAdd.pack()
+
+	# List for the tasks
+	listBox = tk.Listbox(window)
+	listBox.pack()
+
+	# Button to save a Json file
+	buttonSaveJson = tk.Button(window, text="Save to Json", command=buttonSaveJson_click)
+	buttonSaveJson.pack()
 
 	window.mainloop()
 
